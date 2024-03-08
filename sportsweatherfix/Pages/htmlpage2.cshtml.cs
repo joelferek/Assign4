28898f288d4edd55ee66fda3f071770ac625b716
joelferek <jsf00009@mix.wvu.edu>
2024-03-08 11:50:20 -0500

    2

---
+++ sportsweatherfix/Pages/htmlpage2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assign4.Pages
+{
+    public class htmlpage2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
