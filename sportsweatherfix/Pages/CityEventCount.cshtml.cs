b17a802440f1b106f328ed4b5613ffb4b5812570
Samantha Olivio <sbo00003@mail.wvu.edu>
2024-04-10 18:50:34 -0400

    commit razor pg

---
+++ sportsweatherfix/Pages/CityEventCount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assign4.Pages
+{
+    public class CityEventCountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
